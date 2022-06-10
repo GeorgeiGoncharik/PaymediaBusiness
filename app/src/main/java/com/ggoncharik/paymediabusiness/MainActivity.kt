8966c5361b6362df5acf1454216b9c4323ad6175
shapshuk <andrewshapshuk@gmail.com>
2022-06-10 12:38:11 +0300

    firebase setup

--- app/src/main/java/com/ggoncharik/paymediabusiness/MainActivity.kt
+++ app/src/main/java/com/ggoncharik/paymediabusiness/MainActivity.kt
@@ -2,7 +2,11 @@ package com.ggoncharik.paymediabusiness
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import androidx.constraintlayout.motion.widget.Debug.getName
 import com.ggoncharik.paymediabusiness.ui.main.MainFragment
+import com.google.firebase.FirebaseApp
+import com.google.firebase.ktx.Firebase
 
 class MainActivity : AppCompatActivity() {
 
@@ -14,5 +18,9 @@ class MainActivity : AppCompatActivity() {
                 .replace(R.id.container, MainFragment.newInstance())
                 .commitNow()
         }
+
+        val TAG = "MyActivity"
+        Log.i(TAG, FirebaseApp.DEFAULT_APP_NAME)
+
     }
 }
